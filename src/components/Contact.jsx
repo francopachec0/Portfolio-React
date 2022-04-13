bb5732c1a249787bcc95be7f54af33c9f0800bc1
francopachec0 <francopacheco.dev@gmail.com>
2022-04-13 18:17:55 -0300

    form validate

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,9 +8,9 @@ const Contact = () => {
                     <p className='text-4xl font-bold inline border-b-4 border-[#a31621] text-gray-50'>Contáctame</p>
                     <p className='text-gray-50 py-4'>Completa el formulario o envíame un correo - francopacheco.dev@gmail.com</p>
                 </div>
-                <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-                <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-                <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
+                <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' required/>
+                <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' required />
+                <textarea className='bg-[#ccd6f6] p-2' type='text' name="message" rows="10" placeholder='Message' required></textarea>
                 <button className='text-white border-2 hover:bg-[#a31621] hover:border-[#a31621] duration-300 rounded-lg px-4 py-3 my-8 mx-auto flex items-center'>Trabajemos Juntos</button>
             </form>
         </div>
