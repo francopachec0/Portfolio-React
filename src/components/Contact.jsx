e8c630fb574f988787aaf1c6023ac1048dbe88f2
francopachec0 <francopacheco.dev@gmail.com>
2022-04-20 16:22:33 -0300

    cv change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
         <div name='contact' className='w-full h-screen bg-[#0a1128] flex justify-center items-center p-4'>
             <form method='POST' action="https://getform.io/f/645ffd8c-9789-463e-a185-96bf1f503a87" className='flex flex-col max-w-[600px] w-full'>
                 <div className='pb-8'>
-                    <p className='text-4xl font-bold inline border-b-4 border-[#a31621] text-gray-50'>Contactáme</p>
+                    <p className='text-4xl font-bold inline border-b-4 border-[#a31621] text-gray-50'>Contáctame</p>
                     <p className='text-gray-50 py-4'>Completa el formulario o envíame un correo - francopacheco.dev@gmail.com</p>
                 </div>
                 <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' required/>
